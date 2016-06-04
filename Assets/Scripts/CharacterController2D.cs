f6474981862decbb47bdd23f6c4921625b44fed7
jai-vij-180 <yoyojaivij@gmail.com>
2016-06-05 00:52:42 +0530

    initial commit
    initial commit

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController2D : MonoBehaviour {
+
+    public float speed = 5f;
+    Rigidbody2D rb;
+	
+	void Start () {
+       rb = GetComponent<Rigidbody2D>();
+	}
+	
+    
+	void FixedUpdate () {
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+        rb.AddForce( new Vector3( Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed);
+  
+	}
+}
