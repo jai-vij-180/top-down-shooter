330c025b3abb0a8e6db405c69a40ca8ddad99a8d
jai-vij-180 <yoyojaivij@gmail.com>
2016-06-05 02:38:51 +0530

    shooting mechanism added
    bullet and its raycast yet to be coded
    works nice till here lol

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -3,13 +3,44 @@ using System.Collections;
 
 public class CharacterController2D : MonoBehaviour {
 
+    public float shootSpeed = 0.05f;
     public float speed = 5f;
+    float coolDown;
+     public float bulletSpeed = 5000;
+    public Transform gun;
+    public GameObject bulletPrefab;
     Rigidbody2D rb;
 	
 	void Start () {
        rb = GetComponent<Rigidbody2D>();
-	}
+       //gunPoint = new Vector3(gun.transform.position.x, gun.transform.position.y, gun.transform.position.z);
+    }
 	
+    void Update () 
+     {
+     
+         if(Time.time >= coolDown)
+         {
+             if(Input.GetMouseButton(0))
+             {
+                 Fire();
+             }
+         }
+     }
+ 
+     void Fire()
+     {
+
+
+         GameObject bPrefab = Instantiate(bulletPrefab, gun.position, Quaternion.identity) as GameObject;
+
+         bPrefab.GetComponent<Rigidbody2D>().AddForce(transform.up * bulletSpeed);
+ 
+         coolDown = Time.time + shootSpeed;
+     }
+ 
+
+
     
 	void FixedUpdate () {
 
