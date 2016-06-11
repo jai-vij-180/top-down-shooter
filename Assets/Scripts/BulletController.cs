2cdb08751aa7409786435a44e91e51229f61c5be
jai-vij-180 <yoyojaivij@gmail.com>
2016-06-12 04:12:12 +0530

    shooting working
    it works.yaaay

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -1,25 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Mechanism for the bullet and its API
+/// </summary>
 public class BulletController : MonoBehaviour {
+    [Header("Status")]
+    [SerializeField]
+    bool IsInitialized;
+    [Header("Bullet Settings")]
+    public float bulletSpeed = 5000;
+    public float bulletCheckingDistance = 0.05f;
+    public LayerMask WhatDoBulletsHit;
+    public bool AutoDestroy;
+    public float DestroyAfterSeconds=3f;
 
-    public float gunRange = 20f;
-    EnemyController enemyController;
 	void Start () {
-        enemyController = GetComponent<EnemyController>();
+        
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position,Vector3.up, gunRange);
-        Debug.DrawLine(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), Color.green);
-        //Ray ray = new Ray(transform.position, Vector3.forward);
-        //if()
-        //{
-        //    if(hit.collider.tag == "enemy")
-        //        enemyController.health -= 5;
-        //}
-        //Destroy(gameObject, 0.5f);   //to destroy the bullet
-	}
+    void Update()
+    {
+        if (IsInitialized)
+        {
+            BulletHitCheck();
+        }
+            
+   
+    }
+
+
+    void BulletHitCheck()
+    {
+
+        RaycastHit2D hit;
+        hit = Physics2D.Raycast(transform.position, transform.up
+           , bulletCheckingDistance, WhatDoBulletsHit);
+
+        if (hit.collider)
+        {
+           var enemyThatWasHit = hit.collider.gameObject;
+           print("die pls");
+            
+            //Damage enemy here
+        }
+    }
+    #region API
+    /// <summary>
+    /// This method allows others to tell the bullet when to ACTIVATE its functionality, Start runs automatically, we wan which ever script spawns the bullet to be able to activate as needed- ,maybe it would want to do some priliminary checks, if the bullet starts doing its own thing as soon as instantiated, it will be hard to handle.
+    /// </summary>
+    public void InitBullet(){
+        
+        GetComponent<Rigidbody2D>().AddForce(transform.up * bulletSpeed);
+        if (AutoDestroy)
+        {
+            Destroy(this.gameObject, DestroyAfterSeconds);
+        }
+        IsInitialized = true;
+
+    }
+    #endregion
 }
--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -1,52 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CharacterController2D : MonoBehaviour {
-
-    public float shootSpeed = 0.05f;
-    public float speed = 5f;
+/// <summary>
+/// Everything related to controlling the character goes here, NOTHING ELSE, unless its using the API of something else.
+/// </summary>
+public class CharacterController2D : MonoBehaviour
+{
+    [Header("Controller Settings")]
+    public float FireRate = 0.05f;
+    public float WalkSpeed = 5f;
     float coolDown;
-     public float bulletSpeed = 5000;
-    public Transform gun;
+    [Header("References-Manual-Please Drag")]
+    public Transform BulletSpawnTransform;
     public GameObject bulletPrefab;
     Rigidbody2D rb;
-	
-	void Start () {
-       rb = GetComponent<Rigidbody2D>();
-       //gunPoint = new Vector3(gun.transform.position.x, gun.transform.position.y, gun.transform.position.z);
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
     }
-	
-    void Update () 
-     {
-         
-         if(Time.time >= coolDown)
-         {
-             if(Input.GetMouseButton(0))
-             {
-                 Fire();
-             }
-         }
-     }
- 
-     void Fire()
-     {
+
+    void Update()
+    {
+        FireIfApplicable();
+    }
+
+    void FireIfApplicable()
+    {
+        
+        if (Time.time >= coolDown)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                SpawnBullet();
+
+                coolDown = Time.time + FireRate;
+            }
+        }
+    }
+
+    private void SpawnBullet()
+    {
+        var NewSpawnedBullet = Instantiate(bulletPrefab, BulletSpawnTransform.position, BulletSpawnTransform.transform.rotation) as GameObject;
+        NewSpawnedBullet.GetComponent<BulletController>().InitBullet(); //Activate bullets functionailty by using bullets API, after that its upto bullet to handle itself. 
+    }
 
 
-         GameObject bPrefab = Instantiate(bulletPrefab, gun.position, Quaternion.identity) as GameObject;
-
-         bPrefab.GetComponent<Rigidbody2D>().AddForce(transform.up * bulletSpeed);
- 
-         coolDown = Time.time + shootSpeed;
-     }
- 
 
 
-    
-	void FixedUpdate () {
+    void FixedUpdate()
+    {
 
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
-        rb.AddForce( new Vector3( Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed);
-  
-	}
+        rb.AddForce(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * WalkSpeed);
+
+    }
 }
